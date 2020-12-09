6397528a06f3d37adc5eaf19d2ef2c2d2424fbc0
kirill ginko <kirillginko@gmail.com>
2020-12-09 00:28:57 -0500

    added components player functionality and library functionality

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const LibrarySong = ({
+  song,
+  songs,
+  setcurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = () => {
+    setcurrentSong(song);
+    //Add Active State
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    //check if song is playing
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
+  };
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img src={song.cover} alt={song.name}></img>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
